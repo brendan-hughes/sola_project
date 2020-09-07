ae6fe34817a8728d5cfbaf8eee64a31d765bcc62
Brendan Hughes <brendanhughes.dev@gmail.com>
2020-09-07 18:03:21 -0400

    Deployment prep

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
 
 //Connect Database
 connectDB();
 
 //Init Middleware
 app.use(express.json({ extended: false }));
-
-const port = process.env.PORT || 5000;
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
@@ -19,6 +18,18 @@ app.use('/api/nav', require('./routes/api/nav'));
 app.use('/api/contact', require('./routes/api/contact'));
 app.use('/api/charge', require('./routes/api/charge'));
 app.use('/api/admin', require('./routes/api/admin'));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('sola_project/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, 'sola_project', 'build', 'index.html')
+		);
+	});
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
