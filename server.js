00d8fdebaf1238878b775aa3c177f85c6ef8cae1
Brendan Hughes <brendanhughes.dev@gmail.com>
2020-09-10 18:43:09 -0400

    push before host

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('./build'));
 }
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
