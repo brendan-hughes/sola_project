52b4091427fae96ce1c7b2862e340b45ee0837ed
Brendan Hughes <brendanhughes.dev@gmail.com>
2020-09-10 19:02:16 -0400

    push before host

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ app.use('/api/admin', require('./routes/api/admin'));
 
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('./build'));
-}
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('./build'));
+// }
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+// });
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
